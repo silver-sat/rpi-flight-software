08beb915671a9019c7224eddbda5ae96153b92e8
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-08 16:09:42 -0400

    Create axports.satellite.append.sh

---
+++ common/axports.satellite.append.sh
@@ -0,0 +1,5 @@
+# motify /etc/ax25/axports
+sed -e '/^serial/d' /etc/ax25/axports > /tmp/axports
+wget -q -O - 'https://raw.githubusercontent.com/silver-sat/rpi-flight-software/master/common/axports.satellite.append.txt' >> /tmp/axports
+sudo mv -f /tmp/axports /etc/ax25/axports
+sudo chown root.root /etc/ax25/axports
