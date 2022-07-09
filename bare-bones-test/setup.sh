7862e4b86bb918174ec121f510d47651e8ff5fe5
Nathan Edwards <edwardsnj@gmail.com>
2022-07-09 15:16:42 -0400

    fix syntax errors

--- bare-bones-test/setup.sh
+++ bare-bones-test/setup.sh
@@ -5,7 +5,9 @@ sudo apt-get install -y ntpdate
 
 for f in startup.sh network-startup.sh; do
   rm -f /home/pi/$f
-	ln -s $BASE/$f /home/pi/.$f
+  ln -s $BASE/$f /home/pi/.$f
+done
 for d in photo tweet action shutdown; do
   rm -f /home/pi/$d
   ln -s $BASE/$d /home/pi/$d
+done
