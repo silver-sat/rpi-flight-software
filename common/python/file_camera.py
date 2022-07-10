a9b1547e34ece4a126c3e9bd48a059ce8162ef0f
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-10 14:28:09 -0400

    fix bad var name

--- common/python/file_camera.py
+++ common/python/file_camera.py
@@ -9,8 +9,8 @@ def setup_camera():
     pass
 
 def take_photo(filename):
-    from = os.path.join(os.path.split(__file__)[0],'..','etc',standin_photo)
-    shutils.copy(from,filename)
+    photo = os.path.join(os.path.split(__file__)[0],'..','etc',standin_photo)
+    shutils.copy(photo,filename)
 	print("Photo placed in filename:",filename,file=sys.stderr)
 
 
