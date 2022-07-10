bb15d7919b8cbf7441fb43e54918d57195162e23
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-10 15:32:03 -0400

    Update setup.sh

--- bare-bones-test/setup.sh
+++ bare-bones-test/setup.sh
@@ -8,10 +8,16 @@ sudo apt-get install -y ntpdate
 sudo pip3 install twython
 
 cat <<EOF
+
+Run the bootup script manually with:
+
+  sudo runuser -u root sh .startup.sh 
+
 Set photo mode with:
 
   cp rpi-flight-software/common/etc/photo_pins.txt .pins.txt
 	
 Set tweet mode with:
   cp rpi-flight-software/common/etc/photo_pins.txt .pins.txt
+
 EOF
