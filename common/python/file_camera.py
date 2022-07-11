131f1954351e93bf7b82134f4a2089dafaf4484d
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-11 11:23:38 -0400

    Update file_camera.py

--- common/python/file_camera.py
+++ common/python/file_camera.py
@@ -2,15 +2,14 @@
 import shutil
 import os, os.path, sys
 
-# In same directory as module
-standin_photo = 'overhead.jpg'
+photo_location = '/home/pi/.photo.jpg'
 
 def setup_camera():
     pass
 
 def take_photo(filename):
-    photo = os.path.join(os.path.split(__file__)[0],'..','etc',standin_photo)
-    shutil.copyfile(photo,filename)
-    print("Photo placed in filename:",filename,file=sys.stderr)
-
-
+    if os.path.exists(photo_location):
+        shutil.copyfile(photo_location,filename)
+        print("Photo placed in filename:",filename,file=sys.stderr)
+        return True
+    return False
