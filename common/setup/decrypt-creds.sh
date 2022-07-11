92cebc3a9969d18489f7b91f25a84926e23c47a3
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-11 11:54:45 -0400

    Update decrypt-creds.sh

--- common/setup/decrypt-creds.sh
+++ common/setup/decrypt-creds.sh
@@ -6,9 +6,7 @@ if [ -f $BASE/python/twittercred.py.gpg -a ! -s $BASE/python/twittercred.py ]; t
 	stty -echo
         read -p "Encryption password for secret data? " PASSWORD
 	stty echo
-        grep -v '^export PASSWORD=' /home/pi/.params.sh > /home/pi/.params.sh.tmp
-        mv -f /home/pi/.params.sh.tmp /home/pi/.params.sh
-        echo "export PASSWORD='$PASSWORD'" >> /home/pi/.params.sh
+        echo "PASSWORD='$PASSWORD'" >> /home/pi/.params.sh
     fi
     gpg --no-symkey-cache --passphrase "$PASSWORD" --batch -d $BASE/python/twittercred.py.gpg > $BASE/python/twittercred.py
 fi
