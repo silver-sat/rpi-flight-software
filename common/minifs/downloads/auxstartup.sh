49aeb59732c7b983a0bee2b26e13d7a3144e6f8f
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-19 17:31:10 -0400

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -1,3 +1,4 @@
 set -x
 find rpi-flight-software -type f | fgrep -v '/.git/' | md5sum 
-sh ./.minifs/ul.sh .startup.log .startup.log.1
+sh ./.minifs/ul.sh .startup.log
+sh ./.minifs/ul.sh .startup.log.1
