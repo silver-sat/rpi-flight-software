53e98714d00af44f7c9f9a34525f9d74209c0afe
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-22 14:13:36 -0400

    Update ucam_max3100_camera.py

--- common/python/ucam_max3100_camera.py
+++ common/python/ucam_max3100_camera.py
@@ -5,4 +5,5 @@ def setup_camera():
     return uCAM_III_MAX3100(spiDevice=(0,0), 
                             resetPin=23,
                             responseDelay=0,
-                            syncResponseDelay=0.01)
+                            syncResponseDelay=0.01,
+                            verbose=False)
