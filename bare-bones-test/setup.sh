e2c0a9f63b9b1a7637e4c70a149cfe1434b3662e
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-22 11:02:48 -0400

    Update setup.sh

--- bare-bones-test/setup.sh
+++ bare-bones-test/setup.sh
@@ -5,6 +5,9 @@ set -x
 # ntpdate is needed in network-startup.sh
 sudo apt-get install -y ntpdate
 
+rm -f .logrotate.sh
+ln -s ".common/scripts/logrotate.sh" .logrotate.sh
+
 cat <<EOF
 
 Run the bootup script manually with:
