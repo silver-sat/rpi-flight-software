3b8fd5c9d46009e28d066fef89223e4ae8d0ae45
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-07-22 11:15:47 -0400

    Update file_camera.py

--- common/python/file_camera.py
+++ common/python/file_camera.py
@@ -5,9 +5,9 @@ import os, os.path, sys
 photo_location = '/home/pi/.photo.jpg'
 
 def setup_camera():
-    pass
+    return None
 
-def take_photo(filename):
+def take_photo(camera,filename):
     if os.path.exists(photo_location):
         shutil.copyfile(photo_location,filename)
         print("Photo placed in filename:",filename,file=sys.stderr)
