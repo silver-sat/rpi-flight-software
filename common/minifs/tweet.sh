6571e5abb7e853ce446421bf722382840f631692
Nathan Edwards <edwardsnj@users.noreply.github.com>
2022-10-15 14:59:00 -0400

    Update tweet.sh

--- common/minifs/tweet.sh
+++ common/minifs/tweet.sh
@@ -3,7 +3,8 @@ GROUND_IP=$1
 MINIFS_PORT=$2
 MESSAGE=$3
 PHOTO=$4
-if [ -f "$4" ]; then
+if [ "$PHOTO" != "" -a -f "$PHOTO" ]; then
   curl -F "photo=@${PHOTO}" http://${GROUND_ID}:${MINIFS_PORT}/tweet?msg="${MESSAGE}"
+else:
   curl http://${GROUND_ID}:${MINIFS_PORT}/tweet?msg="${MESSAGE}"
 fi
