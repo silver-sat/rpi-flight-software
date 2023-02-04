2afdb3d27d54f48bcc793c139f9277157a132692
AlexGsilversat <alexg@silversat.org>
2023-02-04 16:21:56 -0500

    Update send_reddit_tweet.py

--- common/python/send_reddit_tweet.py
+++ common/python/send_reddit_tweet.py
@@ -11,7 +11,7 @@ def send_text_tweet(reddit,message):
         traceback.print_exc()
     return False
     
-def send_photo_tweet(twitter,message,photo_file):
+def send_photo_tweet(reddit,message,photo_file):
     # photo_file must be on the file system
     try:
         reddit.subreddit(subreddit_name).submit_image(title=message, image_path=photo_file, without_websockets = True)
