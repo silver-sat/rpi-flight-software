02467f01e0a13669cb57c2688feef383135549b0
AlexGsilversat <alexg@silversat.org>
2023-02-04 16:14:43 -0500

    Create send_reddit_tweet.py

---
+++ common/python/send_reddit_tweet.py
@@ -0,0 +1,21 @@
+
+import sys, traceback, json
+
+subreddit_name = "test" #change later
+
+def send_text_tweet(reddit,message):
+    try:
+        reddit.subreddit(subreddit_name).submit(title=message)
+        return True
+    except:
+        traceback.print_exc()
+    return False
+    
+def send_photo_tweet(twitter,message,photo_file):
+    # photo_file must be on the file system
+    try:
+        reddit.subreddit(subreddit_name).submit_image(title=message, image_path=photo_file, without_websockets = True)
+        return True
+    except:
+        traceback.print_exc()
+    return False
