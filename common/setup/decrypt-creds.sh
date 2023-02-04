abc6992cb2ec8cb8e75d5f1f4fcbff34fbc7de6d
Nathan Edwards <edwardsnj@users.noreply.github.com>
2023-02-04 15:41:00 -0500

    Update decrypt-creds.sh

--- common/setup/decrypt-creds.sh
+++ common/setup/decrypt-creds.sh
@@ -8,3 +8,7 @@ if [ -f $COMMON/python/twittercred.py.gpg -a ! -s $COMMON/python/twittercred.py 
   getpasswd "Encryption password for secret data? " PASSWORD
   gpg --no-symkey-cache --passphrase "$PASSWORD" --batch -d $COMMON/python/twittercred.py.gpg > $COMMON/python/twittercred.py
 fi
+if [ -f $COMMON/python/redditcred.py.gpg -a ! -s $COMMON/python/redditcred.py ]; then
+  getpasswd "Encryption password for secret data? " PASSWORD
+  gpg --no-symkey-cache --passphrase "$PASSWORD" --batch -d $COMMON/python/redditcred.py.gpg > $COMMON/python/redditcred.py
+fi
