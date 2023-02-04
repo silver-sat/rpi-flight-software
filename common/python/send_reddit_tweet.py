d2171ebf298c132193792bd7e3f0dcfe94aba070
AlexGsilversat <alexg@silversat.org>
2023-02-04 16:18:20 -0500

    Update send_reddit_tweet.py

--- common/python/send_reddit_tweet.py
+++ common/python/send_reddit_tweet.py
@@ -5,7 +5,7 @@ subreddit_name = "test" #change later
 
 def send_text_tweet(reddit,message):
     try:
-        reddit.subreddit(subreddit_name).submit(title=message)
+        reddit.subreddit(subreddit_name).submit(title=message, selftext='')
         return True
     except:
         traceback.print_exc()
