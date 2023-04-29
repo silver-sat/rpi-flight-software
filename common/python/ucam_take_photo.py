a6835ec2eaa15e3326b0d241ed164277b80b3bb4
Nathan Edwards <edwardsnj@users.noreply.github.com>
2023-04-29 15:24:37 -0400

    max number of connection attempts for camera

--- common/python/ucam_take_photo.py
+++ common/python/ucam_take_photo.py
@@ -3,9 +3,14 @@ import traceback
 import sys
 import time
 
-def take_photo(camera, filename):
+def take_photo(camera, filename, max_attempts=20):
     attempt = 0
     while True:
+    
+        if attempt >= max_attempts:
+            print("Connect w/ camera failed after",attempt,"attempts.",file=sys.stderr)
+            return False
+            
         print("Connect w/ camera, attempt",attempt+1,file=sys.stderr)
         attempt += 1
         camera.reset()
