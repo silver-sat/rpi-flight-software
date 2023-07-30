6672c98be96c73dd8cd7732f1dd917a080b206fe
Nathan Edwards <edwardsnj@users.noreply.github.com>
2023-07-30 12:15:06 -0400

    Update startup.sh

--- payload-board/startup.sh
+++ payload-board/startup.sh
@@ -5,7 +5,8 @@ set -x
 ASPI="runuser -u pi --"
 cd /home/pi
 
-gpio readall
+python3 $COMMOM/python/gpioreadall.py
+
 # Set the SHUTDOWN pins
 $ASPI python3 ./payload/shutdown.py RUNNING
 
@@ -26,14 +27,14 @@ if [ "$MODE" = "PHOTO" ]; then
   sleep 120
   time -p $ASPI python3 -u ./payload/photo.py
   $ASPI python3 ./payload/shutdown.py FINISHED
-  gpio readall
+  python3 $COMMOM/python/gpioreadall.py
   if [ ! -f .noshutdown ] then
 		shutdown -h now
 	fi
 elif [ "$MODE" = "TWEET" ]; then
   time -p $ASPI python3 -u ./payload/tweet.py
   $ASPI python3 ./payload/shutdown.py FINISHED
-  gpio readall
+  python3 $COMMOM/python/gpioreadall.py
   if [ ! -f .noshutdown ] then
 		shutdown -h now
 	fi
