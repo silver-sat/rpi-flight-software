ca126f36e80e92f9216f42c9581372bba39cb3a0
Nathan Edwards <edwardsnj@users.noreply.github.com>
2023-08-09 21:36:36 -0400

    Update tncattach.install.sh

--- common/setup/tncattach.install.sh
+++ common/setup/tncattach.install.sh
@@ -4,7 +4,8 @@ set -x
 
 VERSION="0.1.9"
 URL="https://github.com/markqvist/tncattach/releases/download/${VERSION}/tncattach_${VERSION}_armhf.tar.xz"
-cd
+cd /home/pi
 wget -q -O - "$URL" | xzcat | tar xf -
+rm -f .tncattach
 ln -s tncattach_armhf/tncattach .tncattach
 ls -l .tncattach
