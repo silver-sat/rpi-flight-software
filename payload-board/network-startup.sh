8a6285e1d20e2b1a8f477d81a2994df59a1aec4b
Nathan Edwards <edwardsnj@users.noreply.github.com>
2023-08-09 21:00:54 -0400

    Update network-startup.sh

--- payload-board/network-startup.sh
+++ payload-board/network-startup.sh
@@ -13,7 +13,19 @@ arp -H ax25 -s 192.168.100.101 ${GROUND_CALL}
 sh .logrotate.sh .ax0.log
 axlisten ax0 -a -r -t >/home/pi/.ax0.log 2>&1 &
 
-route del default || true
+GOOD=0
+for i in 1 2 3 4 5 6 7 8 9 10; do
+  route del default
+	sleep 2
+  if [ `route | fgrep default | wc -l` -eq 0 ]; then
+    GOOD=1
+		break
+	fi
+done
+if [ $GOOD -eq 0 ]; then
+  exit 1;
+fi
+
 route add default gw 192.168.100.101 ax0
 
 GOOD=0
