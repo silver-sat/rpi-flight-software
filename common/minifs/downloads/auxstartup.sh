4602320e84a7339fd45b0711df2500f8e971f586
Nathan Edwards <edwardsnj@gmail.com>
2023-08-24 22:23:30 -0400

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -1,7 +1,13 @@
 set -x
-find rpi-flight-software -type f | fgrep -v '/.git/' | xargs -n 10 md5sum 
-find photos -type f | xargs -n 10 md5sum 
+. .params.sh
+# find rpi-flight-software -type f | fgrep -v '/.git/' | xargs -n 10 md5sum 
+# find photos -type f | xargs -n 10 md5sum 
 ls -l photos
-sh ./.minifs/ul.sh 192.168.100.101 5001 .startup.log
-sh ./.minifs/ul.sh 192.168.100.101 5001 .startup.log.1
-sh ./.minifs/ul.sh 192.168.100.101 5001 .startup.log.2
+ifconfig -a
+rfkill list
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.1
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.2
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.3
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.4
+sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.5
