a28bbf184c8e75eb1f50b8e374aeefe531d44a7a
Nathan Edwards <edwardsnj@gmail.com>
2023-08-26 12:57:21 -0400

    Update apt-get.install.sh

--- common/setup/apt-get.install.sh
+++ common/setup/apt-get.install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
+# sudo apt-get update -y
+# sudo apt-get dist-upgrade -y
 
 sudo apt-get install -y git ax25-tools ax25-apps gpg raspi-gpio time rfkill
 
