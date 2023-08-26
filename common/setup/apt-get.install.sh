914c5f769b4c678ffbbec42b1dd4a603794908fb
Nathan Edwards <edwardsnj@gmail.com>
2023-08-26 15:14:17 -0400

    Update apt-get.install.sh

--- common/setup/apt-get.install.sh
+++ common/setup/apt-get.install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# sudo apt-get update -y
+sudo apt-get update -y
 # sudo apt-get dist-upgrade -y
 
-sudo apt-get install -y git ax25-tools ax25-apps gpg raspi-gpio time rfkill
+sudo apt-get install -y git ax25-tools ax25-apps gpg raspi-gpio time rfkill python3-pip
 
 sudo apt -y autoremove
 
