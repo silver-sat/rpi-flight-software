79695e942037832d35f509cf3a5187bfa111e95c
Nathan Edwards <edwardsnj@users.noreply.github.com>
2024-09-21 11:55:17 -0400

    Update startup.sh

--- payload-board/startup.sh
+++ payload-board/startup.sh
@@ -12,6 +12,10 @@ uptime
 # Set the SHUTDOWN pins
 $ASPI python3 ./payload/shutdown.py RUNNING
 
+# Guarantee we are in user-space for at least 30 seconds
+sleep 30 &
+WAITPID=$!
+
 # Set the serial select to RADIO
 $ASPI python3 ./payload/serialselect.py RADIO
 
@@ -21,9 +25,10 @@ MODE=`$ASPI python3 ./payload/mode.py`
 if [ "$MODE" = "TWEET" ]; then
   if ! sh ./payload/network-startup.sh; then
     if [ ! -f .noshutdown ]; then
+      wait $WAITPID
       $ASPI python3 ./payload/shutdown.py FINISHED
       python3 .gpioreadall.py
-	  date
+      date
       uptime
       sleep 5
       shutdown -h now
@@ -38,9 +43,10 @@ if [ "$MODE" = "PHOTO" ]; then
   $ASPI python3 ./payload/serialselect.py CAMERA
   time -p $ASPI python3 -u ./payload/photo.py
   if [ ! -f .noshutdown ]; then
+    wait $WAITPID
     $ASPI python3 ./payload/shutdown.py FINISHED
     python3 .gpioreadall.py
-	date
+    date
     uptime
     sleep 5
     shutdown -h now
@@ -50,9 +56,10 @@ if [ "$MODE" = "PHOTO" ]; then
 elif [ "$MODE" = "TWEET" ]; then
   time -p $ASPI python3 -u ./payload/tweet.py
   if [ ! -f .noshutdown ]; then
+    wait $WAITPID
     $ASPI python3 ./payload/shutdown.py FINISHED
     python3 .gpioreadall.py
-	date
+    date
     uptime
     sleep 5
     shutdown -h now
