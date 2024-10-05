30bdb7c94e617d235eb1da788296e9b97f69f11c
Nathan Edwards <edwardsnj@users.noreply.github.com>
2024-10-05 15:52:11 -0400

    Update pip3.install.sh

--- common/setup/pip3.install.sh
+++ common/setup/pip3.install.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-sudo pip3 install pyserial==3.5 flask==2.3.3 twython==3.9.1 praw==7.7.1 PIL==5.4.1
+sudo python3 -m pip install pyserial==3.5 flask==2.3.3 twython==3.9.1 praw==7.7.1 PIL==5.4.1
