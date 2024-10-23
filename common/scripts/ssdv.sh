75e96f310a406486cc9c0ad4e20fbc32f4bad2dc
Nathan Edwards <edwardsnj@users.noreply.github.com>
2024-10-22 23:42:26 -0400

    Update ssdv.sh

--- common/scripts/ssdv.sh
+++ common/scripts/ssdv.sh
@@ -17,5 +17,5 @@ INDEX=`echo "$IMGFN" | sed -n 's/^.*thumb-\([0-9]*\)\.jpg$/\1/p'`
 INDEX=`expr $INDEX % 256`
 
 SSDV=/home/pi/.ssdvdist/ssdv
-
+echo ssdv -e -n -c \""$SATELLITE_CALL"\" -i \""$INDEX"\" -q \""$SSDVQUAL"\" -l \""$SSDVSIZE"\" \""$IMGFN"\" \""$SSDVFN"\"
 $SSDV -e -n -c "$SATELLITE_CALL" -i "$INDEX" -q "$SSDVQUAL" -l "$SSDVSIZE" "$IMGFN" "$SSDVFN"
