f6735bf57dce61ba2a4a125b6e6c964a1c392aa2
Nathan Edwards <edwardsnj@users.noreply.github.com>
2024-10-22 23:47:33 -0400

    Update photo_ssdv.py

--- common/python/photo_ssdv.py
+++ common/python/photo_ssdv.py
@@ -1,5 +1,7 @@
 
-import os
+import os, os.path
 
 def make_ssdv(imgfn,ssdvfn):
     os.system("sh /home/pi/.ssdv.sh %s %s"%(imgfn,ssdvfn))
+    if os.path.exists(ssdvfn):
+        print("SSDV packets placed in filename:",ssdvfn,file=sys.stderr)
