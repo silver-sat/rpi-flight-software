2a45938dcb3271469ff0982f4b94541090e5daf6
Nathan Edwards <edwardsnj@gmail.com>
2024-10-23 21:30:19 -0400

    Update setup.sh

--- common/setup.sh
+++ common/setup.sh
@@ -10,13 +10,14 @@
 
 set -x
 
-sh $COMMON/setup/apt-get.install.sh
-sh $COMMON/setup/tncattach.install.sh
-sh $COMMON/setup/ssdv.install.sh
-sh $COMMON/setup/pip3.install.sh
-sh $COMMON/setup/rc.local.startup-hook.sh
-sh $COMMON/setup/axports.append.sh
-sh $COMMON/setup/dhcpcd.append.sh
-sh $COMMON/setup/stop.daily.updates.sh
-sh $COMMON/setup/decrypt-creds.sh
-sh $COMMON/setup/git-config.sh
+sh $COMMON/setup/apt-get.install.sh || exit 1
+sh $COMMON/setup/tncattach.install.sh || exit 1
+sh $COMMON/setup/ssdv.install.sh || exit 1
+sh $COMMON/setup/pip3.install.sh || exit 1
+sh $COMMON/setup/rc.local.startup-hook.sh || exit 1
+sh $COMMON/setup/axports.append.sh || exit 1
+sh $COMMON/setup/dhcpcd.append.sh || exit 1
+sh $COMMON/setup/stop.daily.updates.sh || exit 1
+sh $COMMON/setup/decrypt-creds.sh || exit 1
+sh $COMMON/setup/git-config.sh || exit 1
+echo "Done."
