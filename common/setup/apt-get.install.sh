e909a32415e5826c87d160b92fae752cb8b109dc
Nathan Edwards <edwardsnj@gmail.com>
2024-10-24 11:02:37 -0400

    Update apt-get.install.sh

--- common/setup/apt-get.install.sh
+++ common/setup/apt-get.install.sh
@@ -2,12 +2,12 @@
 
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
+# sudo apt-get update -y
+# sudo apt-get upgrade -y
+# sudo apt-get dist-upgrade -y
+# sudo apt-get autoremove -y
 
-sudo apt-get install -y git ax25-tools ax25-apps gpg raspi-gpio time rfkill python3 python3-pip
+sudo apt-get install -y ax25-tools ax25-apps gpg raspi-gpio time rfkill python3 python3-pip
 
 sudo systemctl stop packagekit || true
 sudo systemctl disable packagekit || true
