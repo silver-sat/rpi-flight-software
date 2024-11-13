57919b7d9f54f69113d1833ab90aa18107a03f93
Nathan Edwards <edwardsnj@users.noreply.github.com>
2024-11-13 10:24:45 -0500

    Update setup.sh

--- payload-board/setup.sh
+++ payload-board/setup.sh
@@ -13,7 +13,7 @@ sudo systemctl disable systemd-timesyncd.service
 
 setparamifnotset GROUND_IP 192.168.100.101
 setparamifnotset SATELLITE_IP 192.168.100.102
-setparamifnotset SATELLITE_CALL KC3TTE
+setparamifnotset SATELLITE_CALL KC3VVW
 setparamifnotset KISS_MTU 195
 setparamifnotset BAUD 9600
 setparamifnotset SSDVSIZE 195
