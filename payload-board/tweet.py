d4eee9101dc90ef98e56ec07dcc7b7f51a25554b
Nathan Edwards <edwardsnj@gmail.com>
2024-11-15 10:35:17 -0500

    make more portable and add commented out options for posting

--- payload-board/tweet.py
+++ payload-board/tweet.py
@@ -8,11 +8,14 @@ import find_common_modules
 # the ground station rpi is running the ssltunnel daemon to
 # turn HTTP twitter API use into HTTPS.
 #
+# from direct_bluesky import get_twitter
+# from direct_reddit import get_twitter
 # from direct_tweet import get_twitter
 from proxy_tweet import get_twitter
 
 from photo_files import least_recent_photo, remove_photo
-from tweet_status import make_text_status, make_photo_status
+# from send_bluesky_tweet import send_text_tweet, send_photo_tweet
+# from send_reddit_tweet import send_text_tweet, send_photo_tweet
 from send_tweet import send_text_tweet, send_photo_tweet
 
 filename = least_recent_photo()
