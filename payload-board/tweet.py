49e679074bab2389d61b7ececc07c6b44d496331
Nathan Edwards <edwardsnj@gmail.com>
2024-11-18 19:25:22 -0500

    Move modular tweet options to module

--- payload-board/tweet.py
+++ payload-board/tweet.py
@@ -2,42 +2,17 @@
 import sys
 
 import find_common_modules
+
+from tweet_select import tweet_select
+
 from params import get_params
 
 params = get_params()
-target = params.get('TWEETTARGET','twitter')
 mode = params.get('TWEETMODE','proxy')
+target = params.get('TWEETTARGET','twitter')
 
-assert mode in ("direct","proxy","minifs")
-if mode == "direct":
-    assert target in ("twitter","reddit","bluesky")
-    if target == "twitter":
-        import direct_tweet, send_tweet
-        twitter = direct_tweet.get_twitter()
-        send_text_tweet = send_tweet.send_text_tweet
-        send_photo_tweet = send_tweet.send_photo_tweet
-    elif target == "reddit":
-        import direct_reddit, send_reddit_tweet
-        twitter = direct_reddit.get_twitter()
-        send_text_tweet = send_reddit_tweet.send_text_tweet
-        send_photo_tweet = send_reddit_tweet.send_photo_tweet
-    elif target == "bluesky":
-        import direct_bluesky, send_bluesky_tweet
-        twitter = direct_bluesky.get_twitter()
-        send_text_tweet = send_bluesky_tweet.send_text_tweet
-        send_photo_tweet = send_bluesky_tweet.send_photo_tweet
-elif mode == "proxy":
-    assert target in ("twitter",)
-    import proxy_tweet, send_tweet
-    twitter = proxy_tweet.get_twitter()
-    send_text_tweet = send_tweet.send_text_tweet
-    send_photo_tweet = send_tweet.send_photo_tweet
-elif mode == "minifs":
-    import minifs_tweet, send_minifs_tweet
-    twitter = minifs_tweet.get_twitter()
-    send_text_tweet = send_minifs_tweet.send_text_tweet
-    send_photo_tweet = send_minifs_tweet.send_photo_tweet
-
+twitter, send_text_tweet, send_photo_tweet = tweet_select(mode, target)
+    
 from photo_files import least_recent_photo, remove_photo
 
 filename = least_recent_photo()
