0d4b6eb18ca55c425b2ad199b5dc12ff9adfc580
Nathan Edwards <edwardsnj@gmail.com>
2024-11-18 19:43:24 -0500

    Update pip3.install.sh

--- common/setup/pip3.install.sh
+++ common/setup/pip3.install.sh
@@ -4,5 +4,5 @@ set -x
 
 sudo python3 -m pip install pyserial==3.5 flask==2.3.3 twython==3.9.1 praw==7.8.1
 sudo python3 -m pip install Pillow==8.1.2
-python3 -m pip install --user cryptography==41.0.7
-python3 -m pip install --user atproto==0.0.55
+python3 -m pip install --user --no-warn-script-location cryptography==41.0.7
+python3 -m pip install --user --no-warn-script-location atproto==0.0.55
