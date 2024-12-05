938ab0aa5bd3c0922bd6a77b86795c88f837cd76
Nathan Edwards <edwardsnj@gmail.com>
2024-12-05 09:27:06 -0500

    update use of get_params

--- common/python/proxy_bluesky.py
+++ common/python/proxy_bluesky.py
@@ -2,9 +2,8 @@
 from params import get_param, get_creds
 from bluesky import BlueSky
 
-username,password = get_creds("BLUESKYCRED","username","password")
-
 def get_twitter():
+    creds = get_creds("BLUESKYCRED")
     client = BlueSky(base_url="http://%s:8585/"%(get_param('GROUND_IP'),))
-    client.login(username, password)
+    client.login(creds.username, creds.password)
     return client
