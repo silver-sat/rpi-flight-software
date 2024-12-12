11c8a4d0ed54c6fbe72b159eed3615b9df088908
Nathan Edwards <edwardsnj@gmail.com>
2024-12-12 18:11:54 -0500

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -3,45 +3,29 @@ set -x
 # . .params.sh
 GROUND_IP=192.168.100.101
 
-# sed -i 's/^dtoverlay=vc4-kms-v3d.*/#dtoverlay=vc4-kms-v3d/' /boot/config.txt
-# /usr/bin/tvservice -o
-# rfkill block wifi
-
-# cat <<EOF >>/boot/config.txt
-# dtoverlay=gpio-poweroff,gpiopin=17
-# dtoverlay=gpio-poweroff,gpiopin=27
-# dtoverlay=gpio-poweroff,gpiopin=22
-# EOF
-
-# rm -f .noshutdown
+rm -f .noshutdown
 
 # clear the photo folder
 # rm -f photos/photo-*.jpg
+# rm -f photos/thumb-*.jpg
+# rm -f photos/ssdv-*.bin
 
-# 62265 bytes
-# runuser -u pi -- cp rpi-flight-software/common/etc/overhead.jpg photo-0000100.jpg
+# Copy photo to file photo filename - 62265 bytes
+runuser -u pi -- cp rpi-flight-software/common/etc/overhead.jpg .photo.jpg
 
-# 41762 bytes
-# runuser -u pi -- cp rpi-flight-software/common/etc/group.jpg photo-0000101.jpg
+# set tweetcreds
+runuser -u pi -- setparam.sh TWITTERCRED edwardsnj-ssapp
 
-# Get the small photo
-# runuser -u pi -- sh ./.minifs/dl.sh ${GROUND_IP} 5001 anti-static-mat.jpg rpi-flight-software/common/etc/anti-static-mat.jpg
+# set text tweet...
+rm -f /home/pi/.textstatus.txt
+runuser -u pi -- sh ./.minifs/dl.sh ${GROUND_IP} 5001 textstatus.txt /home/pi/.textstatus.txt
 
-# 8412 bytes
-# runuser -u pi -- cp rpi-flight-software/common/etc/anti-static-mat.jpg photo-0000102.jpg
-
-# post to the silversat account
-# sed -i 's/^TWITTERCRED=.*$/TWITTERCRED="silversat.ssapp"/' /home/pi/.params.sh
-
-# post to the edwardsnj account
-# sed -i 's/^TWITTERCRED=.*$/TWITTERCRED="edwardsnj.ssapp"/' /home/pi/.params.sh
-
-# echo "/home/pi/.params.sh"
-# cat /home/pi/.params.sh
+# set photo tweet...
+rm -f /home/pi/.photostatus.txt
+runuser -u pi -- sh ./.minifs/dl.sh ${GROUND_IP} 5001 photostatus.txt /home/pi/.photostatus.txt
 
 ls -l photos
 
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.1
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.2
-sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.3
