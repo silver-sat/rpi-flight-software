783be1c74dfd70e22faaf1ad5afd77a2d2f2d9fa
Nathan Edwards <edwardsnj@users.noreply.github.com>
2025-02-01 16:42:46 -0500

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -3,6 +3,7 @@ set -x
 GROUND_IP=192.168.100.101
 
 ls -l photos
+ls -l .photo.jpg .textstatus.txt .photostatus.txt
 
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.1
