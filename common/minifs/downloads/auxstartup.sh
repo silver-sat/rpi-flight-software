b3828232f3c540b61a2f602de537869999c22f28
Nathan Edwards <edwardsnj@users.noreply.github.com>
2025-02-08 14:43:37 -0500

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -2,8 +2,16 @@ set -x
 
 GROUND_IP=192.168.100.101
 
+cd /home/pi
 ls -l photos
-ls -l .photo.jpg .textstatus.txt .photostatus.txt
+for f in .params.sh photos/last_photo_index.txt .textstatus.txt .photostatus.txt .photo.jpg; do
+  case $f in 
+    *.txt) echo $f && ls -l $f && cat $f ;;
+    *.sh)  echo $f && ls -l $f && cat $f ;;
+    *.jpg) echo $f && ls -l $f ;;
+  esac
+done
+ls -ld .*.log*
 
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log
 sh ./.minifs/ul.sh ${GROUND_IP} 5001 .startup.log.1
