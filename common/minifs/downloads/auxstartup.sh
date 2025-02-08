b658ef25e638353d8b81a35c7381590ab991a4df
Nathan Edwards <edwardsnj@users.noreply.github.com>
2025-02-08 15:15:16 -0500

    Update auxstartup.sh

--- common/minifs/downloads/auxstartup.sh
+++ common/minifs/downloads/auxstartup.sh
@@ -2,12 +2,17 @@ set -x
 
 GROUND_IP=192.168.100.101
 
+runuser -u pi -- ./setparam.sh TWEETTARGET bluesky
+runuser -u pi -- ./setparam.sh TWEETMODE proxy
+runuser -u pi -- ./setparam.sh BLUESKYCRED edwardsnj
+
 cd /home/pi
 ls -l photos
-for f in .params.sh photos/last_photo_index.txt .textstatus.txt .photostatus.txt .photo.jpg; do
+for f in .params.sh photos/last_photo_index.txt .textstatus.txt .photostatus.txt .photo.jpg .noshutdown; do
   case $f in 
-    *.txt) ls -l $f && cat $f ;;
-    *.sh)  ls -l $f && cat $f ;;
+    *.txt) ;
+    *.sh) ;
+    .noshutdown) ls -l $f && cat $f ;;
     *.jpg) ls -l $f && md5sum $f ;;
   esac
 done
